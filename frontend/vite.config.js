ec1af07dce07a3f94db639685e9f5bc94478b1d0
Swathi1003-sri <swathiarchunan103@gmail.com>
2025-10-30 22:45:42 +0530

    frontendurl

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server:{
-  //   host:"0.0.0.0",
-  //   port:5000,
-  //   allowedHosts:["frontend-9kjh.onrender.com"]
-  // }
+  server:{
+    host:"0.0.0.0",
+    port:5000,
+    allowedHosts:["frontend-ywsh.onrender.com"]
+  }
   
 })
