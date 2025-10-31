a2b127498b344d9369aee659dc4c6d825a28e386
Swathi1003-sri <swathiarchunan103@gmail.com>
2025-10-31 15:47:17 +0530

    commi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     host:"0.0.0.0",
     port:5000,
-    allowedHosts:["frontend-ywsh.onrender.com"]
+    allowedHosts:["frontend-wj34.onrender.com"]
   }
   
 })
