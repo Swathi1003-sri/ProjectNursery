5eefe5b2b0ef9a4e62f47022aa5c45775d49ad5b
Swathi1003-sri <swathiarchunan103@gmail.com>
2025-11-10 11:36:05 +0530

    iuy

--- backend/app.js
+++ backend/app.js
@@ -16,23 +16,8 @@ app.use(express.json({limit:"190mb"}))
 
 // ===================================== CORS =========================================
 
-// const cors = require("cors")
-// app.use(cors())
-
-
-const cors = require("cors");
-
-const allowedOrigins = [
-  "https://frontend-wgsm.onrender.com", // your frontend Render domain
-                
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
+const cors = require("cors")
+app.use(cors())
 
 
 
